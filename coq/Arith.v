64edf0d712521cee1f39f2c31a1aff6541c973ec
Juan García Garland <jpgarcia@fing.edu.uy>
2017-12-06 22:02:17 -0300

    Tarski proof, not quite there yet

--- coq/Arith.v
+++ coq/Arith.v
@@ -347,8 +347,131 @@ Proof.
   assumption.
 Qed.
 
+
+
+Definition Inclusion (X: nat -> Prop) (Y : nat -> Prop) := forall x:nat, X x -> Y x.
+
+Infix "⊂" := Inclusion (at level 50).
+
+
+Definition Fix F := fun x => forall X, (F X ⊂ X) -> X x. 
+
+Definition I F := forall X Y, (X ⊂ Y) -> (F X ⊂ F Y) : Prop.
+
+Definition Eq X Y := X ⊂ Y /\ Y ⊂ X.
+
+Infix "≡" := Eq (at level 40).
+
+
+
+Axiom l1 : forall F, I F
+                     -> F (F (Fix F)) ⊂ F (Fix F) -> Fix F ⊂ F(Fix F).
+
+
+Lemma phi_ff : forall F, I F
+                         -> forall φ : nat -> Prop, F φ ⊂ φ -> Fix F ⊂ φ.
+Proof.
+  intros F HIF φ Hφ.
+  unfold Inclusion.
+  intro n.
+  unfold Fix.
+compute.
+intros.
+apply H.
+intros.
+unfold Inclusion in Hφ.
+apply Hφ.
+assumption.
+Qed.
+
+
+
+Lemma inc_trans : forall X Y Z, X ⊂ Y -> Y ⊂ Z -> X ⊂ Z.
+Proof.
+  intros X Y Z.
+  unfold Inclusion.
+  intros.
+  apply (H0 x (H x H1)).
+Qed.  
+
+Lemma lemma1 : forall F, I F
+                         -> forall φ : nat -> Prop, F φ ⊂ φ -> F (Fix F) ⊂ φ.
+  intros F HIF φ.
+  intro Hφ.
+  cut (Fix F ⊂ φ).
+  Focus 2.
+  unfold Inclusion.
+  unfold Fix.
+  intros.
+  apply (H φ Hφ).
+
+  intros.
+  cut (F (Fix F) ⊂ F φ).
+  intros.
+  apply (inc_trans (F (Fix F)) (F φ) φ).
+  assumption.
+  assumption.
+  cut(Fix F  ⊂ φ).
+  apply HIF.
+  unfold Inclusion.
+  unfold Fix.
+  intros.
+  apply (H0 φ).
+  assumption.
+Qed.
+
+
+(*
+Lemma lemma3 : forall F, I F
+              -> forall φ : nat -> Prop, F φ ⊂ φ
+              -> forall g, g ⊂ φ -> g ⊂ Fix F.
+Proof.
+  intros F HIF φ HFφ g HG.
+  cut (F g ⊂ F φ).
+  intro.
+  cut (F g ⊂ φ).
+  intro.
+  cut (Fix F ⊂ φ).
+  intros.
+  
+
+  unfold Fix.
+  compute.
+  intros n H1 ψ H2.
+  apply H2.
+  unfold Inclusion in HG.
+
+*)
+  
+  
+Lemma tarsky_T1 : forall F, I F -> F (Fix F) ⊂ Fix F.
+Proof.
+  intros F HIF.
+  unfold I in HIF.
+  cut ( F(F (Fix F)) ⊂ Fix F).
+  intros.
+  
+
+
+  
+Lemma tarsky_T2 : forall F, I F -> ((Fix F) ⊂ (F (Fix F))).
+Proof.
+  intros F HIF.
+  apply l1.
+  assumption.
+  apply HIF.
+  apply tarsky_T1.
+  assumption.
+Qed.
+
+
+
+
+
+  Theorem tarsky_fixpoint : forall F, I F -> F (Fix F) ≡ Fix F.
+(* TODO
 Theorem leq_Antisym :
   forall m n : N, m ≤ n -> n ≤ m -> m = n.
 Proof.
   intros m n.
-  intros 
+  intros *)
