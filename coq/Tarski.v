c98dbe9bd7f0a416e8b9c05afc6bf6ee959e863b
Juan García Garland <jpgarcia@fing.edu.uy>
2017-12-21 15:07:31 -0300

    1.0

--- coq/Tarski.v
+++ coq/Tarski.v
@@ -1,15 +1,32 @@
 
+Module Tarski.
+
+(* In this module we use Prop as omicron and builtin nat as iota*)
+  (* We define inclusion and a pretty notation: *)
+  
 Definition Inclusion (X: nat -> Prop) (Y : nat -> Prop)
 := forall x:nat, X x -> Y x.
-
-
 Infix "⊂" := Inclusion (at level 50).
 
+(*Fix: *)
 
 Definition Fix F := fun x => forall X, (F X ⊂ X) -> X x. 
 
+
+(*The definition of an increasing predicate: *)
 Definition I F := forall X Y, (X ⊂ Y) -> (F X ⊂ F Y) : Prop.
 
+(* Transitivity of ⊂ *)
+Lemma inc_trans : forall X Y Z, X ⊂ Y -> Y ⊂ Z -> X ⊂ Z.
+Proof.
+  intros X Y Z.
+  unfold Inclusion.
+  intros.
+  apply (H0 x (H x H1)).
+Qed.  
+
+
+(* Equality, only used as a sintactic sugar at the end, in the main theorem*)
 Definition Eq X Y := X ⊂ Y /\ Y ⊂ X.
 
 Infix "≡" := Eq (at level 40).
@@ -17,8 +34,9 @@ Infix "≡" := Eq (at level 40).
 
 
 
+(*Some previous lemmas: *)
 
-Lemma phi_ff : forall F, I F
+Lemma lemma1 : forall F, I F
                          -> forall φ : nat -> Prop, F φ ⊂ φ -> Fix F ⊂ φ.
 Proof.
   intros F HIF φ Hφ.
@@ -36,15 +54,8 @@ Qed.
 
 
 
-Lemma inc_trans : forall X Y Z, X ⊂ Y -> Y ⊂ Z -> X ⊂ Z.
-Proof.
-  intros X Y Z.
-  unfold Inclusion.
-  intros.
-  apply (H0 x (H x H1)).
-Qed.  
 
-Lemma lemma1 : forall F, I F
+Lemma lemma2 : forall F, I F
                          -> forall φ : nat -> Prop, F φ ⊂ φ -> F (Fix F) ⊂ φ.
   intros F HIF φ.
   intro Hφ.
@@ -83,13 +94,15 @@ Proof.
   assumption.
 Qed.
 
+
+(*Now, Tarski in both directions: *)
 Theorem tarsky_T1 : forall F, I F -> F (Fix F) ⊂ Fix F.
 Proof.
   intros F HIF.  
   apply lemma3.
   assumption.
   intros.
-  apply lemma1.
+  apply lemma2.
   assumption.
   assumption.
 Qed.  
@@ -98,7 +111,7 @@ Lemma l1 : forall F, I F
                      -> F (F (Fix F)) ⊂ F (Fix F) -> Fix F ⊂ F(Fix F).
 Proof.
   intros F HIF H.
-  apply phi_ff.
+  apply lemma1.
   assumption.
   assumption.
 Qed.
@@ -125,4 +138,7 @@ Proof.
   apply tarsky_T2.
   assumption.
 Qed.
+
+
+End Tarski.
 
